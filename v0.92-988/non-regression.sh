f72db7f2440a9697e8cf4d0b309905fa4d936142
Loic Dachary <ldachary@redhat.com>
2015-05-31 12:58:25 +0200

    verify all payloads have been verified
    Collect the directories containing payloads to be verified. At the end
    of the test, if all tests have run, verify that all directories
    containing payload have been used. If not it probably means the tests
    were changed and no longer use these directories. They should be
    removed. It's also a guard against unintended regression that skips
    payloads that should be verified.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- v0.92-988/non-regression.sh
+++ v0.92-988/non-regression.sh
@@ -22,10 +22,35 @@
 : ${JERASURE_VARIANTS:=generic sse3 sse4}
 : ${MYDIR:=--base $(dirname $0)}
 
+TMP=$(mktemp --directory)
+trap "rm -fr $TMP" EXIT
+
+function non_regression() {
+    local action=$1
+    shift
+
+    ceph_erasure_code_non_regression $action "$@" || return 1
+    ceph_erasure_code_non_regression --show-path "$@" >> $TMP/used
+}
+
+function verify_directories() {
+    local base=$(dirname "$(head -1 $TMP/used)")
+    ls "$base" | grep 'plugin=' | sort > $TMP/exist_sorted
+    sed -e 's|.*/||' $TMP/used | sort > $TMP/used_sorted
+    if ! cmp $TMP/used_sorted $TMP/exist_sorted ; then
+        echo "The following directories contain a payload that should have been verified"
+        echo "but they have not been. It probably means that a change in the script"
+        echo "made it skip these directories. If the modification is intended, the directories"
+        echo "should be removed."
+        comm -13 $TMP/used_sorted $TMP/exist_sorted
+        return 1
+    fi
+}
+
 function test_shec() {
     while read k m c ; do
         for stripe_width in $STRIPE_WIDTHS ; do
-            ceph_erasure_code_non_regression --stripe-width $stripe_width --plugin shec --parameter technique=multiple --parameter k=$k --parameter m=$m --parameter c=$c $ACTION $VERBOSE $MYDIR || return 1
+            non_regression $ACTION --stripe-width $stripe_width --plugin shec --parameter technique=multiple --parameter k=$k --parameter m=$m --parameter c=$c $VERBOSE $MYDIR || return 1
         done
     done <<EOF
 1 1 1
@@ -53,7 +78,7 @@ EOF
 function test_lrc() {
     while read k m l ; do
         for stripe_width in $STRIPE_WIDTHS ; do
-            ceph_erasure_code_non_regression --stripe-width $stripe_width --plugin lrc --parameter k=$k --parameter m=$m --parameter l=$l $ACTION $VERBOSE $MYDIR || return 1
+            non_regression $ACTION --stripe-width $stripe_width --plugin lrc --parameter k=$k --parameter m=$m --parameter l=$l $VERBOSE $MYDIR || return 1
         done
     done <<EOF
 2 2 2
@@ -67,7 +92,7 @@ function test_isa() {
         while read k m ; do
             for technique in reed_sol_van cauchy ; do
                 for stripe_width in $STRIPE_WIDTHS ; do
-                    ceph_erasure_code_non_regression --stripe-width $stripe_width --plugin isa --parameter technique=$technique --parameter k=$k --parameter m=$m $ACTION $VERBOSE $MYDIR || return 1
+                    non_regression $ACTION --stripe-width $stripe_width --plugin isa --parameter technique=$technique --parameter k=$k --parameter m=$m $VERBOSE $MYDIR || return 1
                 done
             done
         done <<EOF
@@ -102,15 +127,15 @@ function jerasure_action() {
     local action=$1
     shift
 
-    ceph_erasure_code_non_regression "$@" $action || return 1
+    non_regression $action "$@" || return 1
     if test "$action" = --check ; then
-        path=$(ceph_erasure_code_non_regression "$@" --show-path)
+        path=$(ceph_erasure_code_non_regression --show-path "$@")
         #
         # Verify all variants of the jerasure plugin encode/decode in the same
         # way, although they use a different code path.
         #
         for variant in $(jerasure_variants) ; do
-            ceph_erasure_code_non_regression "$@" --path "$path" --parameter jerasure-variant=$variant --check || return 1
+            ceph_erasure_code_non_regression $action "$@" --path "$path" --parameter jerasure-variant=$variant || return 1
         done
     fi
 }
@@ -176,10 +201,14 @@ EOF
 }
 
 function run() {
-    local funcs=${@:-$(set | sed -n -e 's/^\(test_[0-9a-z_]*\) .*/\1/p')}
+    local all_funcs=$(set | sed -n -e 's/^\(test_[0-9a-z_]*\) .*/\1/p')
+    local funcs=${@:-$all_funcs}
     for func in $funcs ; do
         $func || return 1
     done
+    if test "$all_funcs" = "$funcs" ; then
+        verify_directories || return 1
+    fi
 }
 
 run "$@" || exit 1
